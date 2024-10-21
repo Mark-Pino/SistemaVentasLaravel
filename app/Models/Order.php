7006a010719b41ed115678dfbcc7199f714c864c
Mark-Pino <mark.pino@upeu.edu.pe>
2024-10-20 20:26:03 -0500

    Proyecto

---
+++ app/Models/Order.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class Order extends Model
+{
+    use HasFactory, Sortable;
+
+    protected $fillable = [
+        'customer_id',
+        'order_date',
+        'order_status',
+        'total_products',
+        'sub_total',
+        'vat',
+        'invoice_no',
+        'total',
+        'payment_status',
+        'pay',
+        'due',
+    ];
+
+    public $sortable = [
+        'customer_id',
+        'order_date',
+        'total_products',
+        'invoice_no',
+        'pay',
+        'total',
+        'payment_status',
+        'due',
+    ];
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id', 'id');
+    }
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->whereHas('customer', function ($query) use ($search) {
+                $query->where('name', 'like', '%' . $search . '%');
+            });
+        });
+    }
+}
