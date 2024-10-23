e961907285ce0085d3a2fc8dc9fe4378761bd426
Mark-Pino <mark.pino@upeu.edu.pe>
2024-10-22 21:53:18 -0500

    Proyecto

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -20,16 +20,25 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
-        $user = User::factory()->create();
-
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
+        // Crea un usuario con un nombre de usuario y contraseña
+        $user = User::factory()->create([
+            'username' => 'testuser', // Asegúrate de que se establezca un nombre de usuario
+            'password' => bcrypt('password'), // Asegúrate de que esta contraseña coincida
         ]);
 
+        // Realiza el login usando username y password
+        $response = $this->post('/login', [
+            'username' => $user->username, // Utiliza el nombre de usuario en la solicitud
+            'password' => 'password', // La contraseña debe coincidir
+        ]);
+
+        // Asegúrate de que el usuario esté autenticado
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
     }
+
+
+
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
